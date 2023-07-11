265eff1faf14b63e87d53a40cb6a82da5dab9dd2
Your Name <you@example.com>
2023-07-11 17:46:32 +0300

    add thanks modal

--- js/script.js
+++ js/script.js
@@ -27,24 +27,38 @@ document.addEventListener('DOMContentLoaded', () => {
             modalBg.style.cssText = 'opacity: 1; z-index: 10;';
         })
     })
-    document.querySelector('.modal_close').addEventListener('click', () => document.querySelector('.modal').style.cssText = '');
-    modalBg.addEventListener('click', (e) => {
-        if(e.target === modalBg){
-            document.querySelector('.modal').style.cssText = '';
-        }
-    });
+    document.querySelectorAll('.modal_close').forEach(item => {
+        item.addEventListener('click', () => document.querySelector('.modal').style.cssText = '');
+        modalBg.addEventListener('click', (e) => {
+            if(e.target === modalBg){
+                document.querySelector('.modal').style.cssText = '';
+                if(form.contains(error)){form.removeChild(error)}
+            }
+        });
+    })
     document.addEventListener('keydown', (e) => {
         if(e.key === "Escape" || e.keyCode === 27){
             document.querySelector('.modal').style.cssText = '';
+            if(form.contains(error)){form.removeChild(error)}
         }
     })
     const formName = document.querySelector('[name="name"]'),
           formPhone = document.querySelector('[name="phone"]'),
           formEmail = document.querySelector('[name="email"]'),
-          form = document.querySelector('.feed-form');
+          form = document.querySelector('.feed-form'),
+          modalThanks = document.querySelector('.modal_thanks');
     
     document.querySelector('form').addEventListener('submit', (e) => {
-        e.preventDefault();        
+        e.preventDefault(); 
+        modalBg.querySelector('.modal_dialog').style.display = 'none';
+        modalThanks.style.display = 'flex';
+        e.target.reset();
+        setTimeout(function(){
+            modalThanks.style.display = 'none'; 
+            document.querySelector('.modal').style.cssText = '';         
+                     
+        }, 1500);
+        setTimeout(() => modalBg.querySelector('.modal_dialog').style.display = '', 3000);               
     })
 
 
@@ -52,6 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
           error.classList.add('error');
 
     function validateName(){
+        if(form.contains(error)){form.removeChild(error)}
         error.textContent = 'Укажите корректное имя';
         if(formName.value == '' || formName.value.length < 3){            
             if(form.contains(error)){
@@ -60,10 +75,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 formName.insertAdjacentElement("afterend", error);
             }
         } else {
-            form.removeChild(error);
+            if(form.contains(error)){
+                form.removeChild(error);
+            }
         }
     }
     function validatePhone(){
+        if(form.contains(error)){form.removeChild(error)}
         error.textContent = 'Укажите корректный номер телефона';
         if(formPhone.value == '' || formPhone.value.length < 11){            
             if(form.contains(error)){
@@ -72,12 +90,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 formPhone.insertAdjacentElement("afterend", error);
             }
         } else {
-            form.removeChild(error);
+            if(form.contains(error)){
+                form.removeChild(error);
+            }
         }
     }
     
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     function validateEmail(){
+        if(form.contains(error)){form.removeChild(error)}
         error.textContent = 'Укажите корректный e-mail';
         if(!emailPattern.test(formEmail.value)){            
             if(form.contains(error)){
@@ -86,7 +107,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 formEmail.insertAdjacentElement("afterend", error);
             }
         } else {
-            form.removeChild(error);
+            if(form.contains(error)){
+                form.removeChild(error);
+            }
         }
     }
     
